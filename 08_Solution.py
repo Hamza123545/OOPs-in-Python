a10dfb408e05af1a74af68f36c31d2b4ee3ac319
Muhammad Hamza <150414868+Hamza123545@users.noreply.github.com>
2025-04-20 13:07:15 +0500

    Add files via upload

---
+++ 08_Solution.py
@@ -0,0 +1,70 @@
+#Property decorator and class inheritance and  isinstance function
+
+class Car:
+    total_cars = 0
+    
+    def __init__(self, brand,model):
+        self.__brand = brand
+        self.__model = model
+        Car.total_cars += 1
+
+    def get_brand(self):
+            return self.__brand + " !"
+
+    def full_name(self):
+        return f"{self.__brand} {self.__model}"
+    
+    def fuel_type(self):
+        return "Petro or Diesel"
+    @staticmethod
+    def general_description():
+        return "car is a vehicle that is used for transportation."
+    
+    @property
+    def model(self):
+        return self.__model
+    
+
+class ElectricCar(Car):
+    def __init__ (self, brand, __model, battery_size):
+        super().__init__(brand, __model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric charger"
+
+    
+
+
+# my_car = Car("Toyyota", "Corolla")
+# Car("Honda", "Civic")
+
+# # my_car.__model = "city"
+# print(my_car.model)
+
+
+my_tesla = ElectricCar("Tesla", "Model S", "85K Wh")
+print(isinstance(my_tesla, ElectricCar))
+print(isinstance(my_tesla, Car))
+
+
+# # print(my_tesla.__brand)
+# print(my_tesla.get_brand())
+# print(my_tesla.fuel_type())
+
+
+# honda = Car("Honda", "Civic")
+# print(honda.fuel_type())
+
+
+# print(Car.general_description())
+# print(honda.general_description())
+
+
+# my_car = Car("Toyyota", "Corolla")
+
+# print(my_car.full_name())
+
+# my_new_car = Car("Honda","Civic")
+# print(my_new_car.brand)
+# print(my_new_car.model)
